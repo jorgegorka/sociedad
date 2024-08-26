d2187becb8526a4075ccb952628a8325381d269d
jorgegorka <jorge@alvareznavarro.es>
2024-08-26 13:22:45 +0200

    Upgrade to rails 7.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_08_135113) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_26_112117) do
   create_table "accounts", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
